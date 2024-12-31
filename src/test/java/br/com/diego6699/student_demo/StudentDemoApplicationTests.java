c75e45d5537527123724c77707f819de2554d69f
Diego Leonardo <diegoleonardo9966@gmail.com>
2024-12-31 11:24:04 -0300

    Inicio do projeto

---
+++ src/test/java/br/com/diego6699/student_demo/StudentDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.diego6699.student_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
